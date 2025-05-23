0930eabfcf4fb65174dbf82b937a558e6c7f222e
Kirill <kobza@Mamblz>
2025-05-23 18:15:29 +0300

    Работа над базой данных. Улучшение стабильности бд и добавление новых данных.

--- DesktopProgram/Migrations/20250517073454_InitialCreate.cs
+++ DesktopProgram/Migrations/20250517073454_InitialCreate.cs
@@ -4,10 +4,8 @@
 
 namespace DesktopProgram.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -26,7 +24,6 @@ namespace DesktopProgram.Migrations
                 });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
